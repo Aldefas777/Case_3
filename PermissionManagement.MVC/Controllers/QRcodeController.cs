9dd3ac9dac979f097fc19b4fc14188ef73b04cae
Aldefas777 <mkrvs6@gmail.com>
2022-03-31 15:35:51 +0300

    Добавление генерации qr-Кода

---
+++ PermissionManagement.MVC/Controllers/QRcodeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace PermissionManagement.MVC.Controllers
+{
+    public class QRcodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string inputText)
+        {
+            using (MemoryStream ms=new MemoryStream())
+            {
+                QRCodeGenerator oQRCodeGenerator = new QRCodeGenerator();
+                QRCodeData oQRCodeData = oQRCodeGenerator.CreateQrCode(inputText, QRCodeGenerator.ECCLevel.Q);
+                QRCode oQRCode = new QRCode(oQRCodeData);
+                using (Bitmap oBitmap = oQRCode.GetGraphic(20))
+                {
+                    oBitmap.Save(ms, ImageFormat.Png);
+                    ViewBag.QRCode = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+                return View();
+        }
+    }
+}
